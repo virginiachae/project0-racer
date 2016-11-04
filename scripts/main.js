f0a9a97fa8201ee8a88213f519ed1869c7b10568
virginiachae <chae.virginia@gmail.com>
2016-11-04 16:57:24 -0700

    added winning parameter

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,28 @@
 console.log('linked!');
 
-
 $(window).keypress(function(e){
 	if(e.keyCode === 108) {
-		$('.pink').css({'margin-left': '+=100'});
+		$('.pink').css({'margin-left': '+=100',
+										'opacity' : '+=0.15'});
 		checkWinner();
 } else if (e.keyCode === 97) {
-		$('.blue').css({'margin-left': '+=100'});
+		$('.green').css({'margin-left': '+=100',
+										'opacity' : '+=0.15'});
 		checkWinner();
 	}
 });
 
 function checkWinner(){
-	console.log('checking winner');
+	if ($('.pink').css('margin-left')==='1100px') {
+		//alert('pink wins!');
+		$('body').prepend("<h3>pink wins!</h3>");
+		$('.pink').css({'margin-left': '0'});
+		$('.green').css({'margin-left': '0'});
+	} else if ($('.green').css('margin-left')==='1100px'){
+		//alert('green wins!');
+		$('.container').prepend("<h3>green wins!</h3>");
+		$('.green').css({'margin-left': '0'});
+		$('.pink').css({'margin-left': '0'});
+
+	}
 }
