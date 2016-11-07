3bc30524716d8436e240647b71abcb648883dc19
virginiachae <chae.virginia@gmail.com>
2016-11-07 09:06:37 -0800

    added css

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,17 @@
-console.log('linked!');
-
 $(window).keypress(function(e){
 	if(e.keyCode === 108) {
-		$('.pink').css({'margin-left': '+=100',
-										'opacity' : '+=0.15'});
+		$('.bear').css({'margin-left': '+=100'});
 		checkWinner();
 } else if (e.keyCode === 97) {
-		$('.green').css({'margin-left': '+=100',
-										'opacity' : '+=0.15'});
+		$('.doe').css({'margin-left': '+=100'});
 		checkWinner();
 	}
 });
 
 function checkWinner(){
-	if ($('.pink').css('margin-left')==='1100px') {
-		//alert('pink wins!');
-		$('body').prepend("<h3>pink wins!</h3>");
-		$('.pink').css({'margin-left': '0'});
-		$('.green').css({'margin-left': '0'});
-	} else if ($('.green').css('margin-left')==='1100px'){
-		//alert('green wins!');
-		$('.container').prepend("<h3>green wins!</h3>");
-		$('.green').css({'margin-left': '0'});
-		$('.pink').css({'margin-left': '0'});
-
+	if ($('.bear').css('margin-left')==='1000px') {
+		return 	$('h3').text("bear wins!");
+	} else if ($('.doe').css('margin-left')==='1000px'){
+		return $('h3').text("doe wins!");
 	}
 }
